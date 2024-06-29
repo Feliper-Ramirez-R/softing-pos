af8f3922d08446683276662dd511a6bc5b726841
Feliper-Ramirez-R <feliperamirez0786@gmail.com>
2024-06-29 18:34:15 -0500

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
+if ( false ) {
+  enableProdMode();
+window.console.log = () => {}
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
